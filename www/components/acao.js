35bea3e9eaf0e9a9d76d9bee0897b588c908c362
jussimar <jussimar@jussimarleal.com.br>
2019-10-02 03:32:22 +0900

    criando comunicação com servidor e cadastrando pizza

--- www/components/acao.js
+++ www/components/acao.js
@@ -2,4 +2,28 @@
 
 $(document).on("click","#listar",function(){
   $(location).attr("href","listar.html");
-})
+});
+
+$(document).on("click","#salvar",function(){
+    var parametros ={
+      "sabor":$("#sabor").val(),
+      "valor":$("#valor").val()
+    };
+
+    $.ajax({
+      type:"post",//como vou enviar os dados ao servidor
+      url:"https://appmobile3i2.000webhostapp.com/cadastra.php",//para onde vou enviar
+      data:parametros,//o que eu vou enviar
+      //caso esteja tudo certo executa esse codigo
+      success: function(data){
+        navigator.notification.alert(data);
+        $("#sabor").val("");
+        $("#valor").val("")
+      },
+      //caso algo esteja errado executa esse codigo
+      error: function(data){
+        navigator.notification.alert("Erro ao cadastrar!");
+      }
+    });
+});
+
